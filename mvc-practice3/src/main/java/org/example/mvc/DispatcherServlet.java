544db120db975a2f2c6085d6c2d1605ff031871f
DESKTOP-7QTI9HV\knsol <knsol1992@naver.com>
2024-03-13 10:59:11 +0900

    [패스트캠퍼스] 프런트 컨트롤러 패턴 실습 수강

---
+++ mvc-practice3/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,49 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    @Override
+    public void init() throws ServletException {
+        requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        logger.info("[DispatcherServlet] service started");
+
+        try {
+
+            Controller handler = requestMappingHandlerMapping.findHandler(request.getRequestURI());
+            String viewName = handler.handleRequest(request, response);
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
+
+        } catch (Exception e) {
+
+            logger.info("exception occurred: [{}]", e.getMessage(), e);
+            throw new ServletException();
+
+        }
+
+
+    }
+
+}
